82dce8b5840a47d15bc04cdc385371fa02983eb5
Manikanta Thirupathi <Manikanta Thirupathi>
2016-09-18 17:18:38 +0530

    committed second line

--- A.java
+++ A.java
@@ -6,6 +6,7 @@ Class A
      System.out.println("hai this is git hub yhis is modify");
      System.out.println("enter some lines of code");
      System.out.println("local branch of code line is this is line");
+     System.out.println("local branch of code line is this is secondline");
      
     
 
